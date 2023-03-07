9afe5290c43426323ecfda19c237df712df552ac
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-07 17:40:54 -0300

    sign up implemented, using joi to validate schema

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import signUpSchema from "../schemas/signUpSchema.js";
 import { signUp } from "../controllers/authController.js";
-import { validator } from "../middleware/validator.js";
+import { validator, validateSchema } from "../middleware/validator.js";
 
 const route = Router();
 
-route.post("/signup", validator(signUpSchema), signUp);
+route.post("/signup", validateSchema(signUpSchema), signUp);
 
 export default route;
