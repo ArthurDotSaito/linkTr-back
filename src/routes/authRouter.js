7af2d56c5b362481e4889b95b8c99d5a0528c453
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-07 20:44:44 -0300

    feat: logout route

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import signUpSchema from "../schemas/signUpSchema.js";
 import signInSchema from "../schemas/signInSchema.js";
-import { signUp, signIn } from "../controllers/authController.js";
+import { signUp, signIn, logout } from "../controllers/authController.js";
 import { validator, validateSchema } from "../middleware/validator.js";
 
 const route = Router();
 
 route.post("/signup", validateSchema(signUpSchema), signUp);
 route.post("/signin", validateSchema(signInSchema), signIn);
+route.delete("/logout", logout);
 
 export default route;
