f265f12d3603cdbde71dca452964277192c318dc
davi-mancebo <davimancebo@gmail.com>
2023-03-10 13:55:48 -0300

    correção no get de nomes das treadings

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -2,12 +2,12 @@ import db from "../database/databaseConnection.js"
 
 export async function topTrendings(req, res) {
     const data = await db.query(`
-    SELECT p.name, COUNT(pt.treadingId) as count_treading 
-    FROM trendings p 
-    WHERE p.tradingId = pt.id
-    INNER JOIN postTreads pt ON p.id = pt.treadingId 
-    GROUP BY p.id 
-    ORDER BY count_treading DESC 
+    SELECT t.name, COUNT(pt.tradingId) as count_trading 
+    FROM trendings t 
+    INNER JOIN postTreads pt ON t.id = pt.tradingId 
+    WHERE t.tradingId = pt.id
+    GROUP BY t.id 
+    ORDER BY count_trading DESC 
     LIMIT 10;
     `)
     console.log(data)
