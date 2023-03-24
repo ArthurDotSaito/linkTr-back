6b5969c83067003139f1f27e5c039fa2bfca797a
davi-mancebo <davimancebo@gmail.com>
2023-03-24 18:58:20 -0300

    perf
    refractory of code

--- src/routes/searchUsersRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { searchUsers } from "../controllers/getSearchUsers.js";
-
-const userSearch = Router();
-
-userSearch.get("/search", searchUsers)
-
-export default userSearch;
